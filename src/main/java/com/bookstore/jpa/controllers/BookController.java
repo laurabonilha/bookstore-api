2e8c0eea61c82c35a55c7eff7b2b20c2647287fa
laurabonilha <bonilha.laura@gmail.com>
2025-03-19 11:59:44 -0300

    Adição de DTO e de Services

---
+++ src/main/java/com/bookstore/jpa/controllers/BookController.java
@@ -0,0 +1,10 @@
+package com.bookstore.jpa.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bookstore/books")
+public class BookController {
+
+}
